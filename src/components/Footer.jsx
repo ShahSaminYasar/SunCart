1fa73f796bc191bc693010b5f023e93ae560ea2b
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-05-04 13:17:09 +0600

    Added: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,76 @@
+import { Phone } from "lucide-react";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary text-primary-foreground px-3 py-16 pb-1">
+      <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 md:gap-10">
+        <Link
+          href={"/"}
+          className="text-3xl sm:text-4xl font-semibold text-primary-foreground"
+        >
+          SUNCART
+        </Link>
+
+        <div className="flex flex-col gap-1 text-sm">
+          <span className="text-base font-medium">Follow Us</span>
+
+          {[
+            {
+              label: "Facebook",
+              path: "/",
+            },
+            {
+              label: "Instagram",
+              path: "/",
+            },
+            {
+              label: "YouTube",
+              path: "/",
+            },
+          ].map((link, index) => (
+            <Link href={link?.path} key={link?.path + index}>
+              {link?.label}
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-2 text-sm">
+          <span className="text-base font-medium">Legal</span>
+
+          {[
+            {
+              label: "Privacy Policy",
+              path: "/",
+            },
+            {
+              label: "Terms & Conditions",
+              path: "/",
+            },
+            {
+              label: "Refund Policy",
+              path: "/",
+            },
+          ].map((link, index) => (
+            <Link href={link?.path} key={link?.path + index}>
+              {link?.label}
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-2 text-sm">
+          <span className="text-base font-medium">Our Location</span>
+          <p>Nayasarak, Sylhet 3100, Bangladesh</p>
+          <Link href={"tel:+8801234567890"} className="flex gap-2 items-center">
+            <Phone size={16} /> Call +8801234567890
+          </Link>
+        </div>
+
+        <div className="w-full sm:col-span-2 md:col-span-4 bg-primary text-primary-foreground text-xs font-light text-center py-1">
+          Copyright 2026 &copy; Shah Samin Yasar
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,57 +27,61 @@ const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="w-full h-20 flex items-center justify-center px-3 fixed top-0 left-0 shadow-sm bg-background">
-      <div className="container flex justify-between items-center gap-5">
-        <div className="flex gap-1 items-center">
-          <Button
-            variant="ghost"
-            onClick={() => setMobileMenuOpen((prev) => !prev)}
-            className={"block md:hidden"}
-          >
-            {mobileMenuOpen ? <X size={20} /> : <Menu size={20} />}
-          </Button>
+    <>
+      <header className="w-full h-20 flex items-center justify-center px-3 fixed top-0 left-0 shadow-sm bg-background">
+        <div className="container flex justify-between items-center gap-5">
+          <div className="flex gap-1 items-center">
+            <Button
+              variant="ghost"
+              onClick={() => setMobileMenuOpen((prev) => !prev)}
+              className={"block md:hidden"}
+            >
+              {mobileMenuOpen ? <X size={20} /> : <Menu size={20} />}
+            </Button>
 
-          <Link href={"/"} className="text-3xl font-semibold text-primary">
-            SUNCART
-          </Link>
-        </div>
-
-        <nav
-          className={`bg-background rounded-lg absolute left-3 shadow-sm md:relative md:left-auto md:top-auto p-3 w-[calc(100vw-24px)] transition-all duration-300 ${mobileMenuOpen ? "top-22 opacity-100 pointer-events-auto" : "top-17 opacity-0 pointer-events-none"} md:top-auto md:opacity-100 md:pointer-events-auto md:bg-transparent md:shadow-none md:w-fit`}
-        >
-          <ul
-            className={`text-sm font-medium flex gap-5 items-center justify-center flex-col md:flex-row`}
-          >
-            {navlinks?.map((navlink, index) => (
-              <li
-                key={navlink?.path + index}
-                className={`${pathname === navlink?.path ? "text-primary" : "text-foreground/70 hover:text-foreground"} w-full`}
-              >
-                <Link
-                  href={navlink?.path}
-                  className="block text-center text-nowrap"
-                >
-                  {navlink?.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-
-        <div className="flex gap-1 items-center">
-          <Button asChild variant="ghost">
-            <Link href={"/purchases"}>
-              <ShoppingBag size={20} className="text-foreground" />
+            <Link href={"/"} className="text-3xl font-semibold text-primary">
+              SUNCART
             </Link>
-          </Button>
+          </div>
 
-          <Button asChild>
-            <Link href={"/login"}>Login</Link>
-          </Button>
+          <nav
+            className={`bg-background rounded-lg absolute left-3 shadow-sm md:relative md:left-auto md:top-auto p-3 w-[calc(100vw-24px)] transition-all duration-300 ${mobileMenuOpen ? "top-22 opacity-100 pointer-events-auto" : "top-17 opacity-0 pointer-events-none"} md:top-auto md:opacity-100 md:pointer-events-auto md:bg-transparent md:shadow-none md:w-fit`}
+          >
+            <ul
+              className={`text-sm font-medium flex gap-7 items-center justify-center flex-col md:flex-row`}
+            >
+              {navlinks?.map((navlink, index) => (
+                <li
+                  key={navlink?.path + index}
+                  className={`${pathname === navlink?.path ? "text-primary" : "text-foreground/70 hover:text-foreground"} w-full`}
+                >
+                  <Link
+                    href={navlink?.path}
+                    className="block text-center text-nowrap"
+                  >
+                    {navlink?.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <div className="flex gap-1 items-center">
+            <Button asChild variant="ghost">
+              <Link href={"/purchases"}>
+                <ShoppingBag size={20} className="text-foreground" />
+              </Link>
+            </Button>
+
+            <Button asChild>
+              <Link href={"/login"}>Login</Link>
+            </Button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <div className="mt-20"></div>
+    </>
   );
 };
 export default Header;
