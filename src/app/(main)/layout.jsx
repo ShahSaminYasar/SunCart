9efdd52a4d6c73bf920786e29628303222197b21
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-05-04 12:03:43 +0600

    Initialized Project

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+export default layout;
